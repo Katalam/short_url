3ddd39d98d9ab8edf2a09a7c8724082825acf125
Katalam <b_goerss@icloud.com>
2020-09-27 16:55:30 +0200

    Added short url button

--- url.py
+++ url.py
@@ -5,6 +5,9 @@ import os
 from flask import Flask, request, redirect, render_template, abort, url_for
 import mysql.connector
 from dotenv import load_dotenv
+from urllib.parse import urlparse
+import random
+import string
 load_dotenv()
 
 app = Flask(__name__)
@@ -18,8 +21,17 @@ db = mysql.connector.connect(
 
 c = db.cursor()
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def page():
+    if request.method == "POST":
+        try:
+            url = request.form["url"]
+        except KeyError:
+            abort(400)
+        if validate_url(url):
+            slug = save_db(url)
+            return render_template("done.html", Title = "Short URL", Slug = os.getenv("BASE_URL") + "/s/" + slug)
+        return render_template("error.html", Title = "Error")
     return render_template("new.html", Title = "Short URL")
 
 @app.route("/s/<slug>")
@@ -36,6 +48,55 @@ def page_redirect(slug):
 @app.route("/error")
 def page_error():
     return render_template("error.html", Title = "Error")
+
+"""
+Validates given url.
+"""
+def validate_url(url):
+    try:
+        if url.find(os.getenv("BASE_URL")) > -1:
+            return False
+        result = urlparse(url)
+        return all([result.scheme, result.netloc, result.path])
+    except:
+        return False
+
+"""
+Saves given url in database but return slug if already stored.
+"""
+def save_db(url):
+    c.execute("SELECT slug FROM urls WHERE url = %(url)s;", { "url": url })
+    slug = c.fetchone()
+    if slug is not None:
+        return slug[0]
+    new_slug = get_unique_slug()
+    c.execute("INSERT INTO urls (id, slug, url) VALUES (null, %(slug)s, %(url)s);", { "slug": new_slug, "url": url })
+    db.commit()
+    return new_slug
+
+
+"""
+Generate unique slug.
+"""
+def get_unique_slug():
+    slug = get_random_string()
+    while not isUniqueSlug(slug):
+        slug = get_random_string()
+    return slug
+
+"""
+Returns boolean if random string is already stored as slug.
+"""
+def isUniqueSlug(slug):
+    c.execute("SELECT * FROM urls WHERE slug = %(slug)s;", { "slug": slug })
+    return c.fetchone() is None
+
+
+"""
+Get random 5 char with number string.
+"""
+def get_random_string():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
 
 """
 Init database for first usage.
